4da05b6a8bc053212a119c8764c6af2fe33706b9
Emersonprogr147 <ermesantos573@gmail.com>
2023-10-29 18:45:21 -0300

    added back end

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("teste");
+const contaneir = document.querySelector(".contaneir");
+const qrCodeBtn = document.querySelector("#qr-form button"); 
+
+const qrCodeInput = document.querySelector("#qr-form input");
+const qrCodeImg = document.querySelector("#qr-code img"); 
+/* EVENTO */
+
+function generetiQrCode(){
+  const qrCodeInputValue = qrCodeInput.value ; 
+  console.log(qrCodeInputValue);
+
+  if(!qrCodeInputValue ) return ; 
+  qrCodeBtn.innerText = "Gerando código ...." ; 
+  qrCodeImg.src =`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+  qrCodeImg.addEventListener("load",() =>{
+
+    contaneir.classList.add("active");
+    qrCodeBtn.innerText = "Código criado!" ; 
+  });
+}
+
+qrCodeBtn.addEventListener("click",() =>{
+generetiQrCode();
+});
